837a9842f6c8ad091e8b7391a61e91beb7d4e605
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-08-20 11:11:16 +0530

    Added alert popup about project status

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 //Skeleton loading will work if we render
-
+import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import "./App.css";
@@ -12,11 +12,25 @@ import Incidents from "./pages/Incidents";
 
 function App() {
 
+
+  useEffect(() => {
+    window.alert(
+      "Incident Explorer – Development Preview\n\n" +
+      "This application is currently under active development. " +
+      "Some features are still being implemented and may not be fully functional yet.\n\n" +
+      "The current version demonstrates the implemented UI, API integration, " +
+      "and CognoDB-based architecture.\n\n" +
+      "Thank you for exploring the project!"
+    );
+  }, []);
+
+
+
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <BrowserRouter>
-    
+
 
       {/* <div
         style={{
@@ -42,14 +56,14 @@ function App() {
 
         <Routes>
 
-        {/* <Dashboard /> */}
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/incidents" element={< Incidents />} />
+          {/* <Dashboard /> */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/incidents" element={< Incidents />} />
 
         </Routes>
 
       </div>
-      
+
 
     </BrowserRouter>
   );
